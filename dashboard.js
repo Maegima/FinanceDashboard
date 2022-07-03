41fc837c11c2506ae54b254b909377bc32399144
maegima <andre.maegima@unifesp.br>
2022-07-02 22:03:27 -0300

    Added selection for type update

--- dashboard.js
+++ dashboard.js
@@ -122,44 +122,77 @@ $(function () {
         return true;
     }
 
-    // $('#finance-body').on("click", "tr", function() {
-    //     var nodeList;
-    //     if(selected_row != undefined && this != selected_row){
-    //         current_data = [];
-    //         nodeList = selected_row.childNodes;
-    //         for(var [ index, value ] of nodeList.entries()) {
-    //             if(nodeList[index].children.length > 0){
-    //                 var item;
-    //                 nodeList[index].innerHTML = item = nodeList[index].children[0].value;
-    //                 if(typeof row_data[index] == "number"){
-    //                     item = parseFloat(item);
-    //                 }
-    //                 current_data.push(empty(item) ? row_data[index] : item);
-    //             }
-    //         }
-    //         //console.log(current_data);
-    //         datatable.row(selected_row).data(current_data);
-    //     }
+    function changeField(cell){
+        var columnId = cell.index().column;
+        var column = cell.column(columnId).header().innerHTML.toLowerCase();
+        node = cell.node();
         
-    //     _mirror = $('#autosize');
-    //     if(this != selected_row){
-    //         nodeList = this.childNodes;
-    //         for(var [ index, value ] of nodeList.entries()) {
-    //             value = nodeList[index].innerHTML;
-    //             nodeList[index].innerHTML = `<input value="${value}" />`;
-    //             _mirror.text(value);
-    //             _input = $(nodeList[index].childNodes[0]);
-    //             //_input.width( _mirror.width());
-    //         }
-    //         datatable.columns.adjust().draw();
-    //         selected_row = this;
-    //         row_data = datatable.row(this).data();
-    //     }
-    // })
+        switch(column){
+            case "type":
+                var typeSelect = $("#finance-form").find("select[name=type]");
+                var input = document.createElement("select");
+                typeSelect.children().each(function(){
+                    if(this.innerHTML == cell.data())
+                        input.innerHTML += `<option selected="selected" value=${this.value}>${this.innerHTML}</option>`
+                    else
+                        input.innerHTML += `<option value=${this.value}>${this.innerHTML}</option>`
+                });
+                node.innerHTML = input.outerHTML;
+                var input = node.children[0];
+
+                $(input).on("change", changeUpdate);
+                $(input).trigger('mousedown');
+                break;
+            default:
+                var input = document.createElement("input");
+                input.setAttribute("value", node.innerHTML);
+                const end = input.value.length;
+                input.setSelectionRange(end, end);
+                node.innerHTML = input.outerHTML;
+                var input = node.children[0];
+
+                $(input).on("keyup", keyupUpdate);
+                break;
+        }
+        
+        input.focus();
+    }
+
+    function changeUpdate(){
+        if(empty(this.value)){
+            col_data = "null";
+            col_disp = null;
+        }
+        else if(!isNaN(this.value)){
+            col_data = parseFloat(this.value);
+            col_disp = this.options[this.selectedIndex].text;
+        }
+        else{
+            col_data = `"${this.value}"`;
+            col_disp = this.options[this.selectedIndex].text;
+        }
+        this.blur();
+    }
+
+    function keyupUpdate(e){
+        if(e.key == "Enter"){
+            if(empty(this.value)){
+                col_data = "null";
+                col_disp = null;
+            }
+            else if(!isNaN(this.value)){
+                col_data = parseFloat(this.value);
+                col_disp = parseFloat(this.value);
+            }
+            else{
+                col_data = `"${this.value}"`;
+                col_disp = this.value
+            }
+            this.blur();
+        }
+    }
+
     $('#finance-body').on("click", "td", function() {
-        //console.log(datatable.cell(this).column().header().innerHTML);
-        //console.log(datatable.row(this).node());
-        //console.log(this);
         if(this != selected_col){
             var node;
             if(selected_col != undefined){
@@ -168,43 +201,19 @@ $(function () {
                 node.innerHTML = data;
             }
             var cell = datatable.cell(this);
-            node = datatable.cell(this).node();
-            node.innerHTML = `<input value="${node.innerHTML}" />`;
-            const input = node.children[0];
-            input.focus();
-            const end = input.value.length;
-            input.setSelectionRange(end, end);
+            changeField(cell);
             selected_col = this;
-            col_disp = datatable.cell(this).data();
-
-            $(input).on("keyup", function(e){
-                if(e.key == "Enter"){
-                    if(empty(this.value)){
-                        col_data = "null";
-                        col_disp = null;
-                    }
-                    else if(!isNaN(this.value)){
-                        col_data = parseFloat(this.value);
-                        col_disp = parseFloat(this.value);
-                    }
-                    else{
-                        col_data = `"${this.value}"`;
-                        col_disp = this.value
-                    }
-                    this.blur();
-                }
-            })
+            col_disp = cell.data();
         }
     }) 
 
     $('#finance-body').on("focusout", "td", function() {
         if(this == selected_col){
             var cell = datatable.cell(selected_col);
-            if(col_disp != cell.data()){
+            if(col_disp != cell.data() && col_data !== undefined && col_disp !== undefined){
                 var row = datatable.row(selected_col);
                 var col = cell.index().column;
                 id = row.data()[0];
-                var display = col_disp;
                 key = cell.column(col).header().innerHTML.toLowerCase();
                 data = `{"${key}": ${col_data}}`;
                 $.ajax({
@@ -213,19 +222,23 @@ $(function () {
                     type: "PUT",
                     dataType: "json" 
                 }).done((response) =>{
-                    cell.data(display);
+                    cell.data(col_disp);
                 }).fail((xhr, textStatus, errorThrown) => {
                     cell.node().innerHTML = cell.data();
                     datatable.draw();
                     alert(xhr.responseText);
+                }).always(() => {
+                    col_disp = undefined;
+                    col_data = undefined;
+                    selected_col = undefined;
                 })
             }
             else{
-                cell.node().innerHTML = col_disp;
+                var data = datatable.cell(selected_col).data();
+                var node = datatable.cell(selected_col).node();
+                node.innerHTML = data;
+                selected_col = undefined;
             }
-            col_disp = undefined;
-            col_data = undefined;
-            selected_col = undefined;
         }
     })
 
